8de50c67a12de5f99e4d598c692fbe18847af44a
Hugo <hugo-pires@outlook.com>
2016-06-25 13:22:19 -0300

    tests

--- yore-api/src/test/java/com/teles/yore/api/tests/ApiTests.java
+++ yore-api/src/test/java/com/teles/yore/api/tests/ApiTests.java
@@ -2,45 +2,74 @@ package com.teles.yore.api.tests;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.mock.env.MockEnvironment;
 
+import com.jayway.restassured.RestAssured;
 import com.teles.yore.api.verticle.v1.YoreImageVerticle;
+import com.teles.yore.domain.api.YoreImage;
+import com.teles.yore.domain.api.YoreRequest;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.json.Json;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 
 @RunWith(VertxUnitRunner.class)
 public class ApiTests {
 
+	private static final String API_V1_IMAGE = "/api/v1/image";
 	private Vertx vertx;
-	private int port;
-	private String resource;
 
 	@Before
 	public void setup(TestContext context) throws IOException {
 		this.vertx = Vertx.vertx();
 
 		ServerSocket socket = new ServerSocket(0);
-		this.port = socket.getLocalPort();
+		int newPort = socket.getLocalPort();
 		socket.close();
 
-		this.resource = "http://localhost:" + this.port + "/api/v1";
-
 		MockEnvironment env = new MockEnvironment();
-		env.setProperty("server.port", this.port + "");
-		env.setProperty("yore.api.route.pixelate", "/api/v1/image");
+		env.setProperty("server.port", newPort + "");
+		env.setProperty("yore.api.route.pixelate", API_V1_IMAGE);
 
 		this.vertx.deployVerticle(new YoreImageVerticle(env), context.asyncAssertSuccess());
+
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = newPort;
 	}
 
 	@Test
 	public void testValidAndSuccesfulPixelate(TestContext context) throws Exception {
-		
+		YoreRequest yoreRequest = createRequest();
+		YoreImage yoreImage = RestAssured.given().body(Json.encodePrettily(yoreRequest)).request().post(API_V1_IMAGE).andReturn().as(YoreImage.class);
+
+		Assert.assertNotNull(yoreImage);
+		Assert.assertTrue(yoreImage.getImage() != null || yoreImage.getSize() > 0);
+		Assert.assertEquals(yoreRequest.getYoreImage().getName(), yoreImage.getName());
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		RestAssured.reset();
+	}
+
+	private static YoreRequest createRequest() throws IOException {
+		YoreImage yoreImage = new YoreImage();
+		yoreImage.setName("sandy.jpg");
+		yoreImage.setImage(Files.readAllBytes(Paths.get("src/test/resources/photo.jpg")));
+
+		YoreRequest yoreRequest = new YoreRequest();
+		yoreRequest.setPixelSize(6);
+		yoreRequest.setYoreImage(yoreImage);
+		return yoreRequest;
 	}
 
 }
