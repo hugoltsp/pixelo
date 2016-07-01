b6e5e3434d2f83f561b46ec1b69fb536c58604e7
Hugo Pires <hugo-pires@outlook.com>
2016-07-01 15:10:31 -0300

    webapp

--- yore-app/src/main/resources/static/app/app.js
+++ yore-app/src/main/resources/static/app/app.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('appController', [ '$scope', function($scope) {
+angular.module('app', []).controller('appController', [ '$scope', function($scope) {
 
 	$scope.request = {
 		yoreImage : {
