2d5e2121b9b941da166de8b3da2943dbfde58ebb
Hugo <hugo-pires@outlook.com>
2016-07-10 18:52:53 -0300

    slider

--- yore-app/src/main/resources/static/app/app.js
+++ yore-app/src/main/resources/static/app/app.js
@@ -1,23 +1,39 @@
-angular.module('app', ['flow']).config(['flowFactoryProvider', function (flowFactoryProvider) {
-	  flowFactoryProvider.defaults = {
-			    target: 'upload.php',
-			    permanentErrors: [404, 500, 501],
-			    maxChunkRetries: 1,
-			    chunkRetryInterval: 5000,
-			    simultaneousUploads: 4,
-			    singleFile: true
-	  };
-	  flowFactoryProvider.on('catchAll', function (event) {
-		  		console.log('catchAll', arguments);
-	  });
-	}]).controller('appController', [ '$scope', function($scope) {
-		
-		$scope.request = {
-			yoreImage : {
-				image : null,
-				name : null
+'use strict'
+
+angular.module('app', [ 'flow' ]).config(['flowFactoryProvider', function(flowFactoryProvider) {
+		flowFactoryProvider.defaults = {
+			target : 'upload.php',
+			permanentErrors : [ 404, 500, 501 ],
+			maxChunkRetries : 1,
+			chunkRetryInterval : 5000,
+			simultaneousUploads : 4,
+			singleFile : true
+		};
+		flowFactoryProvider.on('catchAll', function(event) {
+			console.log('catchAll', arguments);
+		});}]).controller('appController', [ '$scope', function($scope) {
+
+	var init = function(){
+		var slider = document.getElementById('pixelSizeSlider');
+		noUiSlider.create(slider,{
+			start: 5,
+			orientation: 'vertical',
+			connect: "lower",
+			range: { 
+				'min': 1,
+				'max': 10
 			},
-			pixelSize : 5
-		}
-		
-}]);
+		});
+	}
+	
+	init();
+			
+	$scope.request = {
+		yoreImage : {
+			image : null,
+			name : null
+		},
+		pixelSize : 5
+	}
+
+} ]);
