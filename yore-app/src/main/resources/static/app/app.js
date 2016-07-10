5608cfe1763a279eba939d0bfd61ec6ae1de85f0
Hugo <hugo-pires@outlook.com>
2016-07-10 17:10:23 -0300

    web page

--- yore-app/src/main/resources/static/app/app.js
+++ yore-app/src/main/resources/static/app/app.js
@@ -1,11 +1,23 @@
-angular.module('app', []).controller('appController', [ '$scope', function($scope) {
-
-	$scope.request = {
-		yoreImage : {
-			image : null,
-			name : null
-		},
-		pixelSize : 5
-	}
-
-} ]);
+angular.module('app', ['flow']).config(['flowFactoryProvider', function (flowFactoryProvider) {
+	  flowFactoryProvider.defaults = {
+			    target: 'upload.php',
+			    permanentErrors: [404, 500, 501],
+			    maxChunkRetries: 1,
+			    chunkRetryInterval: 5000,
+			    simultaneousUploads: 4,
+			    singleFile: true
+	  };
+	  flowFactoryProvider.on('catchAll', function (event) {
+		  		console.log('catchAll', arguments);
+	  });
+	}]).controller('appController', [ '$scope', function($scope) {
+		
+		$scope.request = {
+			yoreImage : {
+				image : null,
+				name : null
+			},
+			pixelSize : 5
+		}
+		
+}]);
