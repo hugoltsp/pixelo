2554c89c0cf2a9ecf15285a406ade31aeff04896
Hugo <hugo-pires@outlook.com>
2016-07-16 00:39:42 -0300

    slider

--- yore-app/src/main/resources/static/app/app.js
+++ yore-app/src/main/resources/static/app/app.js
@@ -14,15 +14,22 @@ angular.module('app', [ 'flow' ]).config(['flowFactoryProvider', function(flowFa
 		});}]).controller('appController', [ '$scope', function($scope) {
 
 	var init = function(){
+		var pixelSizeValueElement = document.getElementById('pixelSizeValue');
 		var slider = document.getElementById('pixelSizeSlider');
+		
 		noUiSlider.create(slider,{
 			start: 5,
 			orientation: 'vertical',
 			connect: "lower",
+			step: 1,
 			range: { 
 				'min': 1,
 				'max': 10
 			},
+		});
+		
+		slider .noUiSlider.on('update', function( values, handle ) {
+			pixelSizeValueElement.innerHTML = values[handle];
 		});
 	}
 	
